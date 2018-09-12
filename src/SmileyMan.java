bfc5dd28299d8e76411a9c4fb8ae85bea3119394
akaur20 <42978513+akaur20@users.noreply.github.com>
2018-09-12 14:08:05 -0400

    Add files via upload

---
+++ src/SmileyMan.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author Avleen Kaur Period 7
+ * Smiley Face
+ *
+ */
+
+public class SmileyMan 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("\\\\\\\\\\\\\\\\");
+		System.out.println("| *  * |");
+		System.out.println("|   *  |");
+		System.out.println("|  \\_/ |");
+		System.out.println("\\______/ ");
+		
+		
+
+	}
+
+}
